55d61cb13c815941c3288d2324a1feae73c46429
Laura Wacker <lwacker@cogolabs.com>
2014-04-27 14:17:00 -0400

    transfered to use openpyxl library, rather than xlswrite, xlsread libraries.

--- pos_parser/position_parser.py
+++ pos_parser/position_parser.py
@@ -4,6 +4,12 @@ import os
 import csv
 from xlrd import open_workbook, empty_cell
 from xlwt import Workbook
+
+from openpyxl import Workbook as wb
+from openpyxl.cell import get_column_letter
+
+
+
 from datetime import date
 import logging
 
@@ -14,6 +20,25 @@ from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
 from pdfminer.cmapdb import CMapDB
 from pdfminer.layout import LAParams
 
+
+	
+######################################################################################################
+######################################################################################################
+######### SETUP GLOBAL VARS 
+
+#real expressions that need to be compiled
+re_spaces = re.compile(" +")
+re_contract = re.compile("[A-Z][A-Z][A-Z]\d\d")
+re_settlement = re.compile("Settlement")
+re_avg = re.compile("Avg")
+re_curr = re.compile("USD")		
+re_stop = re.compile("Total Open Trade Equity")
+
+#necessary constants 
+account_0_number = 'PIO79640'
+account_1_number = 'PIO79646'
+account_2_number = "PIO79647"
+account_3_number = "PIO79648"
 
 ######################################################################################################
 ######################################################################################################
@@ -26,10 +51,23 @@ month_dictionary = {1:'JAN', 2:'FEB', 3:'MAR', 4:'APR', 5:'MAY', 6:'JUN', 7:'JUL
 # creates data structure to store the positions found
 def create_model(): 
 	account_dict = {} 
-	account_dict['P79640'] = {'GOLD' : [], 'SILVER' : [], 'EURO': []}
-	account_dict['P79646'] = {'GOLD' : [], 'SILVER' : [], 'EURO': []}
-	account_dict['P79647'] = {'GOLD' : [], 'SILVER' : [], 'EURO': []}
+	account_dict[account_0_number] = {'GOLD' : [], 'SILVER' : [], 'EURO': []}
+	account_dict[account_1_number] = {'GOLD' : [], 'SILVER' : [], 'EURO': []}
+	account_dict[account_2_number] = {'GOLD' : [], 'SILVER' : [], 'EURO': []}
 	return account_dict
+
+# formatting for openpyxl retartedness 
+def get_letter_val_openpyxl(_int):
+
+	return get_column_letter(_int)
+
+
+# removes + sign from positons that are posititve	
+def reformat_position_amount(position_amount):
+	if '+' in position_amount: 
+		return position_amount[1:]
+	else: 
+		return position_amount
 
 
 # gets the list of global contracts based on time of year, and the year
@@ -101,26 +139,15 @@ def get_recent_pdf(logging_flag):
 	# this shouldn't happen, but if it does, raise exception
 	raise Exception('neither the pdf ' + file_str_2 + ' or the pdf ' +  file_str_1 +  " exists in this directory- try entering the file you would like to parse manually")
 
-	
+
+
+
 ######################################################################################################
 ######################################################################################################
-######### SETUP GLOBAL VARS 
+########### CREATE THE MODEL 
 
 global_contracts = get_global_contracts() 
 
-#real expressions that need to be compiled
-re_spaces = re.compile(" +")
-re_contract = re.compile("[A-Z][A-Z][A-Z]\d\d")
-re_settlement = re.compile("Settlement")
-re_avg = re.compile("Avg")
-re_curr = re.compile("USD")		
-re_stop = re.compile("Total Open Trade Equity")
-
-#necessary constants 
-account_0_number = 'P79640'
-account_1_number = 'P79646'
-account_2_number = "P79647"
-account_3_number = "P79648"
 
 ######################################################################################################
 ######################################################################################################
@@ -147,7 +174,7 @@ def find_positions(fin):
 	search_content = False
 	acct_change = False
 	
-	current_account = 'P79640'
+	current_account = account_0_number
 	while (line < len(data_list)):
 		next_line = data_list[line]
 
@@ -195,6 +222,7 @@ def find_positions(fin):
 					search_content = False
 		
 				if (currency != None and contract != None) : 
+					
 					contract_str = contract.group()
 			
 					if re.search("GOLD", next_line):
@@ -204,7 +232,7 @@ def find_positions(fin):
 					elif re.search("IMM EURO", next_line):
 							con_type = "EURO"
 					else:
-							raise ValueError("You have some strange contract type that is neither Gold, Siler or Eurodollars in your account")
+							raise ValueError("You have some strange contract type that is neither Gold, Siler or Eurodollars in your account %s" % current_account)
 					settlement_search  = True
 					contract_search  = False 
 		
@@ -229,10 +257,10 @@ def find_positions(fin):
 				# checking for mishaps - silver in the gold account, gold in the silver account 
 				if (current_account == account_1_number and con_type=='SILVER'):
 					logging.warn("AHHH You bone head!")
-					logging.warn("You have a position of " + contract_str + " " + pos_str + " silver in the Gold (P79646) account!")
+					logging.warn("You have a position of " + contract_str + " " + pos_str + " silver in the Gold %s account!" % account_1_number)
 				elif (current_account == account_2_number and con_type=="GOLD"): 
 					logging.warn("Asleep at the trading desk again?")
-					logging.warn("You have a position of " + contract_str + " " + pos_str + " gold in the Silver (P79647) account!")
+					logging.warn("You have a position of " + contract_str + " " + pos_str + " gold in the Silver %s account!" % account_2_number)
 
 
 				contract_search = True
@@ -250,17 +278,30 @@ def find_positions(fin):
 ############ FUNCTIONS THAT CREATE THE EXCEL DOCUMENT
 
 def create_trading_sheet(positions, global_workbook):
-	trade_sheet = global_workbook.add_sheet('Formatted Positions', cell_overwrite_ok=True)
+	#trade_sheet = global_workbook.add_sheet('Formatted Positions', cell_overwrite_ok=True)
+	trade_sheet = global_workbook.create_sheet()
+	trade_sheet.title = "Formatted Positions"
 
-	trade_sheet.write(0, 0, "Contract")
-	trade_sheet.write(0, 1, "GOLD, 646")
-	trade_sheet.write(0, 2, "EURO, 646")
-	trade_sheet.write(0, 3, "SILV, 647")
-	trade_sheet.write(0, 4, "EURO, 647")
+	#trade_sheet = global_workbook.add_sheet('Formatted Positions', cell_overwrite_ok=True)
+	trade_sheet.cell('%s%s' % ('A',1)).value = 'Contract'
+	trade_sheet.cell('%s%s' % ('B',1)).value = 'GOLD 646'
+	trade_sheet.cell('%s%s' % ('C',1)).value = 'EURO 646'
+	trade_sheet.cell('%s%s' % ('D',1)).value = 'SILV 647'
+	trade_sheet.cell('%s%s' % ('E',1)).value = 'EURO 647'
+
+
+	# trade_sheet.write('A', 1, "Contract")
+	# trade_sheet.write('B', 2, "GOLD, 646")
+	# trade_sheet.write('C', 3, "EURO, 646")
+	# trade_sheet.write('D', 4, "SILV, 647")
+	# trade_sheet.write('E', 5, "EURO, 647")
 	
 	def contract_iterate(positions, column_number):
+
+		trade_sheet.cell('%s%s' % ('A',1)).value = 'Contract'
+		column_letter = get_letter_val_openpyxl(column_number)
 		pos_counter = 0 
-		global_counter = 0
+		global_counter = 1
 		position_length = len(positions)
 	#loop to iterate over the positions
 		while (global_counter < len(global_contracts)):
@@ -272,57 +313,83 @@ def create_trading_sheet(positions, global_workbook):
 
 				if (contract== pos_contract):
 				#write the contract label col. (+1 because of the heading) 
-					trade_sheet.write(global_counter+1, 0, contract)
+					trade_sheet.cell('%s%s' % ('A', global_counter+1)).value = contract
+
+					#trade_sheet.write(global_counter+1, 0, contract)
 				# write the contract value 
-					trade_sheet.row(global_counter+1).set_cell_number(column_number, pos_amount)
+					trade_sheet.cell('%s%s' % (column_letter, global_counter + 1)).value=reformat_position_amount(pos_amount)
+					#trade_sheet.row(global_counter+1).set_cell_number(column_number, pos_amount)
 				#position is next in line, iterate both counters
 					pos_counter +=1
 					global_counter +=1
 				else: 
-					trade_sheet.write(global_counter+1, 0, global_contracts[global_counter])
-					trade_sheet.row(global_counter+1).set_cell_number(column_number, 0)
+					trade_sheet.cell('%s%s' % ('A', global_counter + 1 )).value=global_contracts[global_counter]
+					trade_sheet.cell('%s%s' % (column_letter, global_counter + 1)).value=0
+					#trade_sheet.write(global_counter+1, 0, global_contracts[global_counter])
+					#trade_sheet.row(global_counter+1).set_cell_number(column_number, 0)
 					global_counter +=1
 
 			else:
 				while (global_counter < len(global_contracts)): 	
-					trade_sheet.write(global_counter+1, 0, global_contracts[global_counter])
-					trade_sheet.row(global_counter+1).set_cell_number(column_number, 0)
+					trade_sheet.cell('%s%s' % ('A', global_counter+1)).value = global_contracts[global_counter]
+					trade_sheet.cell('%s%s' % (column_letter, global_counter + 1)).value= 0 
+
+					#trade_sheet.write(global_counter+1, 0, global_contracts[global_counter])
+					#trade_sheet.row(global_counter+1).set_cell_number(column_number, 0)
 					global_counter +=1
 
-	gold_positions = positions['P79646']['GOLD']
-	euro1_positions = positions['P79646']['EURO']
-	silver_positions = positions['P79647']['SILVER']
-	euro2_positions = positions['P79647']['EURO']
+	gold_positions = positions[account_1_number]['GOLD']
+	euro1_positions = positions[account_1_number]['EURO']
+	silver_positions = positions[account_2_number]['SILVER']
+	euro2_positions = positions[account_2_number]['EURO']
 		
-	contract_iterate(gold_positions, 1)
-	contract_iterate(euro1_positions, 2)
-	contract_iterate(silver_positions,3)
-	contract_iterate(euro2_positions, 4)
+	contract_iterate(gold_positions, 2)
+	contract_iterate(euro1_positions, 3)
+	contract_iterate(silver_positions,4)
+	contract_iterate(euro2_positions, 5)
 
 	return global_workbook
 
 
 def create_reference_sheet(positions, global_workbook): 
-		reference_sheet = global_workbook.add_sheet('Positions by Account', cell_overwrite_ok = True)
+		
+		ws1 = global_workbook.create_sheet() 
+		reference_sheet = global_workbook.create_sheet()
+		reference_sheet.title = "Positions by Account"
 
 		# account break down 
-		column = 0
+		column = 1
 		for account, commodities in positions.items(): 
-			row = 0
+			row = 1
 			# writes the acct number
-			reference_sheet.write(0, column, account)
+			letter = get_letter_val_openpyxl(column)
+			reference_sheet.cell('%s%s'%(letter, 1)).value = account
 			
+			#reference_sheet.write(0, column, account)
+			print column
+			print "before 2nd for loop"
 			for c_type, pos_list in commodities.items(): 
-				reference_sheet.write(1, column, c_type)
-				row=2
+				letter = get_letter_val_openpyxl(column)
+				reference_sheet.cell('%s%s' % (letter,2)).value =  c_type
+				#reference_sheet.write(1, column, c_type)
+				row=3
 				# go down a row 
 				if pos_list == {}: 
 					column +=2
 				
 				for position in pos_list:
-					reference_sheet.write(row, column, position[0])
+
+					letter = get_letter_val_openpyxl(column)
+				
+					reference_sheet.cell('%s%s' % (letter,row)).value =  position[0]
+					#reference_sheet.write(row, column, position[0])
 					column +=1 
-					reference_sheet.write(row, column, position[2])
+					letter = get_letter_val_openpyxl(column)
+
+					# if positive position, take out the "+" sign 
+					position_amount = reformat_position_amount(position[2])
+					reference_sheet.cell('%s%s' % (letter,row)).value = position_amount
+					#reference_sheet.write(row, column, position[2])
 					column -=1
 					row +=1 
 				
@@ -331,7 +398,7 @@ def create_reference_sheet(positions, global_workbook):
 		return global_workbook
 
 def populate_workbook(positions):
-	global_book = Workbook()
+	global_book = wb()
 	global_book = create_reference_sheet(positions, global_book)
 	global_book = create_trading_sheet(positions, global_book)
 	try: 
